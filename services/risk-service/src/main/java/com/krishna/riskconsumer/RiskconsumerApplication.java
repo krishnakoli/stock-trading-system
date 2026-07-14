7d4ca2799594d85afd7d99e767a15c400875d120
Krishna <krishna@Krishnas-MacBook-Pro.local>
2026-07-14 21:43:15 +0530

    feat(risk-service): bootstrap Spring Boot application

---
+++ services/risk-service/src/main/java/com/krishna/riskconsumer/RiskconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.riskconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskconsumerApplication.class, args);
+	}
+
+}
