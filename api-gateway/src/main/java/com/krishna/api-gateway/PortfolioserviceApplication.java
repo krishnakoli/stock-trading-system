60baea4a26325499466384f256290c5e1df0fcb3
Krishna <krishna@Krishnas-MacBook-Pro.local>
2026-08-07 15:22:55 +0530

    feat(api-gateway): bootstrap Spring Cloud Gateway

---
+++ api-gateway/src/main/java/com/krishna/api-gateway/PortfolioserviceApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.api-gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioserviceApplication.class, args);
+	}
+
+}
