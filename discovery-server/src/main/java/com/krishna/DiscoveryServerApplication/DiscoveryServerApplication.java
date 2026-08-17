36cabba0a9e2997ce17c71538d5af7614ea74d0f
Krishna <krishna@Krishnas-MacBook-Pro.local>
2026-08-17 20:25:54 +0530

    feat(discovery): add Eureka service discovery server

---
+++ discovery-server/src/main/java/com/krishna/DiscoveryServerApplication/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.DiscoveryServerApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ discovery-server/src/test/java/com/krishna/DiscoveyServerApplicationTest/DiscoverServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.krishna.DiscoveyServerApplicationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveyServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
